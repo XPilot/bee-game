5b30d2d27ac034290628c5653df2edb44254dd88
Radu Paraschiv <xpilot@gmail.com>
2016-08-07 18:18:02 +0300

    Add ai

---
+++ src/app/ai/bee.ai.js
@@ -0,0 +1,19 @@
+// acts as an interface for the bee
+
+class BeeAi {
+  constructor(reference) {
+    this.ref = reference;
+  }
+
+  attack(dmg) {
+
+  }
+
+  defend(dmg) {
+
+  }
+
+  isAllowedToMove(gridX, gridY) {
+
+  }
+}
