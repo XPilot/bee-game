6caa3b3f12eebbfbbdd9337042d0b0b97f135f5d
Radu Paraschiv <xpilot@gmail.com>
2016-08-09 09:39:18 +0300

    Added  more fine tuning to the app

--- src/app/constants/constants.js
+++ src/app/constants/constants.js
@@ -1,14 +1,53 @@
+// the size of the board
+// use square boards (don't fuck things up)
+// if board is rectancle, bees may look funny :)
+// you've been warned
+
 const grid = {
   rows: 5,
   columns: 5
 };
 
-const bees = 10;
-const wasps = 10;
+
+// the number of bees in the game
+// (aligned from edges, never cenered)
+const bees = 5;
+const wasps = 5;
+
+// generic stats of bees
+const beeStats = {
+  health: 100,
+  attack: 30,
+  defense: 30,
+};
+
+const beeQueenStats = {
+  health: 100,
+  attack: 50,
+  defense: 50,
+};
+
+// generic stats of wasps
+const waspStats = {
+  health: 100,
+  attack: 30,
+  defense: 30,
+};
+
+const waspQueenStats = {
+  health: 100,
+  attack: 50,
+  defense: 50,
+};
+
 
 export default {};
 export {
   grid,
   bees,
   wasps,
+  beeStats,
+  beeQueenStats,
+  waspStats,
+  waspQueenStats,
 };
