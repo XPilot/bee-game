6f9059aa971676d432a5ae5d0ca320aed0369224
Radu Paraschiv <xpilot@gmail.com>
2016-08-09 09:35:52 +0300

    Add bee AI

--- src/app/ai/bee.ai.js
+++ src/app/ai/bee.ai.js
@@ -1,19 +1,22 @@
 // acts as an interface for the bee
+import { beeStats, beeQueenStats, waspStats, waspQueenStats } from 'constants/constants';
+
+const getLucky = () => Math.floor(Math.random() * (100 + 1));
 
 class BeeAi {
-  constructor(reference) {
-    this.ref = reference;
-  }
-
-  attack(dmg) {
-
-  }
-
-  defend(dmg) {
-
-  }
-
-  isAllowedToMove(gridX, gridY) {
-
+  static generateStats(type) {
+    switch (type) {
+      case 'bee':
+        return Object.assign({}, beeStats, { luck: getLucky() });
+      case 'bee-queen':
+        return Object.assign({}, beeQueenStats, { luck: getLucky() });
+      case 'wasp':
+        return Object.assign({}, waspStats, { luck: getLucky() });
+      case 'wasp-queen':
+        return Object.assign({}, waspQueenStats, { luck: getLucky() });
+      default: return {};
+    }
   }
 }
+
+export default BeeAi;
